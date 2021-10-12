b1953bcf9911da251fcf7ee8f758e27eb7a3c42a
Vasiliy <88965041+TheLeirby@users.noreply.github.com>
2021-10-12 14:15:18 +0300

    fix: Binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,228 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js'),
 
-// const { Node } = require('../extensions/list-tree.js');
+ { Node } = require('../extensions/list-tree.js');
 
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 module.exports = class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // return root node of the tree
+
+
+
+  root() { 
+
+    return this.treeRoot; 
+
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  // add node with data to the tree
+
+  add(data) {    
+
+    if (this.treeRoot == null) {
+
+      this.treeRoot = new Node(data);
+
+    } else {
+
+      addData(this.treeRoot, new Node(data));
+
+    } 
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  //* returns true if node with the data exists in the tree and false otherwise
+
+  has(data) {
+
+    let node = this.find(data);    
+
+    return ! (node == null);
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  //*returns node with the data if node with the data exists in the tree and null otherwise
+
+  find(data) {
+
+    return findNodeData(this.treeRoot, data) 
+
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  //*removes node with the data from the tree if node with the data exists
+
+  remove(data) {
+
+    this.treeRoot = removeDate(this.treeRoot, data);    
+
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+
+  //*returns minimal value stored in the tree (or null if tree has no nodes)
+
+  min() {    
+
+    return minNode(this.treeRoot).data;   
+
   }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+
+  //*max — returns maximal value stored in the tree (or null if tree has no nodes)
+
+  max() {             
+
+    return maxNode(this.treeRoot).data;
+
+  }
+
+
+
+  constructor() { 
+
+    this.treeRoot = null; 
+
   }
 
 }
+
+
+
+function findNodeData(node, data) {           
+
+  if(node == null) return null;
+
+    else if(data < node.data) {
+
+      return findNodeData(node.left, data);
+
+    } else if(data > node.data) {
+
+      return findNodeData(node.right, data);
+
+    } else {
+
+      return node;
+
+    }
+
+}
+
+
+
+function addData(parentNode, newNode) {
+
+  if (newNode.data < parentNode.data){
+
+    if (parentNode.left == null) {
+
+      parentNode.left = newNode;
+
+    } else {
+
+      addData(parentNode.left, newNode);
+
+    }
+
+  } else if (parentNode.right == null) {
+
+    parentNode.right = newNode;
+
+  } else addData(parentNode.right, newNode)
+
+  
+
+}
+
+
+
+function removeDate(node, key) {  
+
+  if (node == null) {
+
+    return null;
+
+  } else if (key > node.data) {
+
+    node.right = removeDate(node.right, key);
+
+    return node;
+
+  } else if (key < node.data) {
+
+    node.left = removeDate(node.left, key);
+
+    return node;
+
+  } else {
+
+    if (node.right == null && node.left === null) {
+
+      node = null;
+
+      return node;
+
+    } else if (node.right == null) {
+
+      node = node.left;
+
+      return node;
+
+    } else if (node.left == null) {
+
+        node = node.right;
+
+        return node;
+
+      }  
+
+  }
+
+  node.data = minNode(node.right).data; 
+
+  node.right = removeDate(node.right, minNode(node.right).data);
+
+  return node;
+
+}
+
+
+
+function minNode(node) {
+
+  if(node.left == null) {
+
+    return node;
+
+  } else {
+
+    return minNode(node.left);
+
+  }
+
+}
+
+
+
+function maxNode(node) {
+
+  if(node.right == null) {
+
+    return node;
+
+  } else {
+
+    return maxNode(node.right);
+
+  }
+
+} 
+
