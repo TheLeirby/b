1d304578a8d75852268e8adb765b667f12f85e2e
Vasiliy <88965041+TheLeirby@users.noreply.github.com>
2021-10-14 11:23:25 +0300

    fix: (ST) Queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { NotImplementedError } = require('../extensions/index.js'),
+  { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +12,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
 
+module.exports = class Queue {
+  queue = new Array();
+  
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (Object.keys(this.queue).length == 0) {
+      this.queue = new ListNode(value);
+    } else {
+      var saveCurrent = this.queue;
+      while (saveCurrent.next) {
+        saveCurrent = saveCurrent.next;
+      }
+      saveCurrent.next = new ListNode(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    var saveCurrent = this.queue.value;
+    this.queue.value = this.queue.next.value;
+    this.queue.next = this.queue.next.next;
+    return saveCurrent;
   }
 
 }
